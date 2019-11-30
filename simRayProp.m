eafcf6af43c6b8e7737420449966f6366431327e
peyton-gozon <peyton.gozon@wustl.edu>
2019-11-29 20:58:52 -0600

    same as before.

--- simRayProp.m
+++ simRayProp.m
@@ -11,5 +11,13 @@
 % theta_out - list of rays' angles in yz plane at output
 
 function [y_out,theta_out] = simRayProp(M,y_in,theta_in)
-
+    % length(y_in) gives us N, where |y_in|, |theta_in|, |y_out|, and
+    % |theta_out| are all Nx1 vectors. 
+    y_out     = zeros(length(y_in), 1);
+    theta_out = zeros(length(y_in), 1);
+    for i = 1:length(y_in)
+        x = M * [y_in theta_in].';
+        y_out(i) = x(1);
+        theta_out(i) = x(2);
+    end
 end
